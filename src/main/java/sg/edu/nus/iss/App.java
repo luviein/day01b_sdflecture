91015b9af9ee596a56db550a6ff72afe6fa505fd
Yenleng <yenlengggggg@gmail.com>
2023-04-17 17:00:05 +0800

    minor change

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,31 @@
 package sg.edu.nus.iss;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Console cons = System.console();
+
+        List<String> todos = new ArrayList<String>();
+
+        String input = "";
+
+        while (!input.equals("quit")) {
+            input = cons.readLine("Enter a String: ");
+            if(!input.equals("quit")){
+                todos.add(input);
+            } 
+            
+
+        }
+        for(String todo:todos){
+            System.out.println("Todo task: " + todo);
+        }
     }
 }
